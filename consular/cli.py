f1bbb49b64c09a4c7341fac0509756a8cd31e5a8
Simon de Haan <simon@praekeltfoundation.org>
2015-07-13 08:50:57 +0200

    document command line params

--- consular/cli.py
+++ consular/cli.py
@@ -2,8 +2,10 @@ import click
 
 
 @click.command()
-@click.option('--host', default='localhost')
-@click.option('--port', default='7000', type=int)
+@click.option('--host', default='localhost',
+              help='The host to bind to.')
+@click.option('--port', default='7000', type=int,
+              help='The port to listen to.')
 @click.option('--consul', default='http://localhost:8500',
               help='The Consul HTTP API')
 @click.option('--marathon', default='http://localhost:8080',
